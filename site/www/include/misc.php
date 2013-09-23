8725cfbcc98f6a4b97f59d3efb33110738dca837
Jerome Guilloux <jguilloux@genymobile.com>
2013-09-23 08:29:22 +0200

    Split promotion and welcome blocks in seperated files

---
+++ site/www/include/misc.php
@@ -0,0 +1,8 @@
+<div class="grid_4 block-2">
+  <article class="block-2-content">
+    <h4>Promotion</h4>
+    <p class="color-3">Offre d'ouverture exceptionelle</p>
+    <p class="color-4">Pour l'ouverture du salon, une r&eacute;duction exceptionnelle de 20% sur l'ensemble des prestations vous est offerte.</p>
+	<a href="code-promotion-ouverture.php" class="button-1">Imprimer l'offre</a>
+  </article>
+</div>
--- site/www/index.php
+++ site/www/index.php
@@ -71,25 +71,10 @@
                   </div>  
                 </div>
             </div>
-            <div class="grid_8 block-1">
-            	<article class="block-1-content">
-                	<h3>Bienvenue !</h3>
-                    <p class="color-1">New styles in hair cuts, hair color, make up services.</p>
-                    <p class="color-2">Architecto beatae naseaeraciat kuiase undeosler miasosac usa ntuedearsteked icexplic
-abo nemo enim ipsam lertasevol uptatemia voluptas sit. Aspernatur aut odit aut fugitedera
-quia consequuntur magni dolores eosqui ratione voluptatem. Fequi nesciunt neque porro 
-quisquam estqui dolorem ipsumquia dolor sitamet.</p>
-                </article>
-            </div>
-            <div class="grid_4 block-2">
-            	<article class="block-2-content">
-            		<h4>Promotion</h4>
-                    <p class="color-3">15.12.2011 Lertasera Miaseras</p>
-                    <p class="color-4">unde omasler miase lerosac cntiureuede earsteked icta su expline enim ipsam
-ertase volupta quia voluptasit.</p>
-					<a href="#" class="button-1">En savoir plus</a>
-				</article>
-            </div>
+
+            <?php require('include/welcome.php'); ?>
+            <?php require('include/misc.php'); ?>
+
             <div class="grid_4">
             	<article class="block-3 maxheight">
                     <h5>Nos Horaires</h5>
