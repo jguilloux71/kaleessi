74ad6844e82b79ce896058f2a95a8dbe489d32d3
Jerome Guilloux <jguilloux@genymobile.com>
2013-09-28 11:35:58 +0200

    Links for Facebook and Twitter

--- site/www/include/retrieve-us.php
+++ site/www/include/retrieve-us.php
@@ -2,8 +2,8 @@
   <article class="block-5">
     <h5>Retrouvez-nous</h5>
     <ul class="list-1">
-      <li><a href="#">Facebook</a></li>
-      <li><a href="#">Twitter</a></li>
+      <li><a href="https://www.facebook.com/Kaleessi">Facebook</a></li>
+      <li><a href="https://twitter.com/Kaleessi71">Twitter</a></li>
     </ul>
   </article>
 </div>
