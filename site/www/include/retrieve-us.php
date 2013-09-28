372b68a3e67bc4956891d51724fdc8a2cdff2bf9
Jerome Guilloux <jguilloux@genymobile.com>
2013-09-28 14:13:01 +0200

    Target "blank" for facebook and Twitter links

--- site/www/include/retrieve-us.php
+++ site/www/include/retrieve-us.php
@@ -2,8 +2,8 @@
   <article class="block-5">
     <h5>Retrouvez-nous</h5>
     <ul class="list-1">
-      <li><a href="https://www.facebook.com/Kaleessi">Facebook</a></li>
-      <li><a href="https://twitter.com/Kaleessi71">Twitter</a></li>
+      <li><a href="https://www.facebook.com/Kaleessi" target="_blank">Facebook</a></li>
+      <li><a href="https://twitter.com/Kaleessi71" target="_blank">Twitter</a></li>
     </ul>
   </article>
 </div>
