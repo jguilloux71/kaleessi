285dc070bf2613767cdc813810c6ee6766c1fd86
Jerome Guilloux <jguilloux@genymobile.com>
2013-09-28 14:03:03 +0200

    Promotion

--- site/www/include/misc.php
+++ site/www/include/misc.php
@@ -3,6 +3,6 @@
     <h4>Promotion</h4>
     <p class="color-3">Offre d'ouverture exceptionelle</p>
     <p class="color-4">Pour l'ouverture du salon, une r&eacute;duction exceptionnelle de 20% sur l'ensemble des prestations vous est offerte.</p>
-	<a href="code-promotion-ouverture.php" class="button-1">Imprimer l'offre</a>
+    <a href="images/kaleessi/reduction-20-percent.png" target="_blank" class="button-1">Imprimer l'offre</a>
   </article>
 </div>
