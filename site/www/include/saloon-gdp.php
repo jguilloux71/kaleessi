3444b740a6443a443b320f657b6a92a0fd111521
Jerome Guilloux <jguilloux@genymobile.com>
2013-10-02 21:18:08 +0200

    Saloon GDP

---
+++ site/www/include/saloon-gdp.php
@@ -0,0 +1,22 @@
+<div id="gdp" class="grid_12 page-2-row-1">
+  <h2 class="pad-2">GRAINE DE PLAISIR</h2>
+  <div class="wrapper">
+    <img src="images/graine-de-plaisir/logo-graine-de-plaisir-full.png" alt="" class="img-indent-2">
+    <div class="extra-wrap">
+      <p class="color-5">Laissez la magie Graine de Plaisir enchanter votre quotidien et réveiller vos sens en voyageant dans notre univers où tous les rêves sont permis...</p>
+      <p>Kaleessi vous fait découvrir sa gamme de cosmétiques Graine de Plaisir : toujours plus innovants et glamours, les gels, fluides et bougies Graine de Plaisir vous procureront des sensations encore plus troublantes.</p>
+      <p>En partenariat avec des laboratoires français de renom, Graine de Plaisir a créé des produits haut de gamme aux senteurs de fruits et de fleurs pour laisser votre peau douce et gourmande. Ce sont des  formules sans alcool, sans phénoxyéthanol et sans paraben.</p>
+    </div>
+      <br/>
+      <br/>
+      <br/>
+    <div class="extra-wrap">
+      <p class="color-5">Devenez ambassadrice Graine de Plaisir</p>
+      <p>Kaleessi vous permet de devenir Vendeuse à Domicile Indépendante (VDI) pour la marque Graine de Plaisir : vous souhaitez arrondir vos fin de mois tout en vous amusant, n’hésitez pas à rejoindre Graine de Plaisir, un kit de démarrage, des formations, pas d’obligation de chiffre, des cadeaux, tout est réuni pour bien démarrer et se développer.</p>
+      <p>Graine de Plaisir, c’est aussi plus de 200 ambassadrices dans toutes la France.</p>
+      <br/>
+      <p class="color-5">Vous souhaitez découvrir toute la gamme de produits Graine de Plaisir</p>
+      <p>Contactez-nous pour organiser une soirée shopping-sexy entre copine et découvrez toute une gamme de produits coquins, chics et glamour haut de gamme, de la lingerie fine, des accessoires, des jeux de couple, des jouets intimes, tout pour pimenter votre vie de couple, qui vous sera présentée par une ambassadrice.</p>
+    </div>
+  </div>
+</div>
---
+++ site/www/include/saloon-kaleessi.php
@@ -0,0 +1,41 @@
+<div class="grid_8 page-2-col-1">
+  <h2 class="pad-1">KALEESSI</h2>
+  <div class="wrapper">
+    <img src="images/kaleessi/kaleessi-pink-black-512.png" alt="" class="img-indent" width="100" />
+    <div class="extra-wrap">
+      <p class="color-8">Kertyade Fertasera Miaseras Lertyas</p>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada.</p>
+<p>&nbsp;</p>
+<p>Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan.</p>
+    </div>
+  </div>
+
+  <div class="wrapper">
+    <ul class="list-2">
+      <li><a href="#">Ultrices posuere cubilia Curae</a></li>
+      <li><a href="#">Suspendisse sollicitudin</a></li>
+      <li><a href="#">Ut pharetra augue nec augue</a></li>
+    </ul>
+    <ul class="list-2">    
+      <li><a href="#">Lacinia fermentum</a></li>
+      <li><a href="#">Donec in velit vel ipsum auctor</a></li>
+      <li><a href="#">Vestibulum iaculis lacinia est</a></li>
+    </ul>
+  </div>
+  <p>Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+</div>
+
+<div class="grid_4">
+  <div class="page-2-banner">
+    <div class="page-2-banner-img">
+      <div><strong>2011</strong></div>
+    </div>
+    <div class="page-2-banner-text">
+      <div>
+        <strong class="col-1">New</strong><strong class="col-2"> Hairstyle</strong>
+        <p class="color-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit raesent vestibulum.</p>
+      </div>    
+    </div>
+  </div>
+</div>
+
