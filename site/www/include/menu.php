58e72d449f07ef1e490c6f9e850fa8a570ffad5b
Jerome Guilloux <jguilloux@genymobile.com>
2013-10-03 19:04:59 +0200

    'Download' submenu to download prices

--- site/www/include/menu.php
+++ site/www/include/menu.php
@@ -1,7 +1,11 @@
 <nav>
   <ul class="sf-menu shadow">
     <li><a href="index.php">Accueil</a></li>
-    <li><a href="prices.php">Prestations/Tarifs</a></li>
+    <li><a href="prices.php">Prestations/Tarifs</a>
+      <ul>
+        <li><a href="data/prices.pdf">Telecharger</a></li>
+      </ul>
+    </li>
     <li><a href="saloon.php">Salon</a>
       <ul>
         <li><a href="photos.php">Photos</a></li>
