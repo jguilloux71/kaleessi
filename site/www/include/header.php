9043133d66db4bba0084b5d0772e02f1f4c1d9ee
Jerome Guilloux <jguilloux@genymobile.com>
2013-10-19 10:47:27 +0200

    Newsletter + privacy with CNIL

--- site/www/include/header.php
+++ site/www/include/header.php
@@ -1,5 +1,10 @@
 <title>Kaleessi</title>
+
 <meta charset="utf-8">
+<meta name="Copyright" content="Kaleessi" />
+<meta name="description" content="Kaleessi est un salon moderne et innovant où la prothésie ongulaire côtoie le plaisir et le bien-être" />
+<meta name="keywords" content="stylisme ongulaire, ongles, ongle, pose, pose ongle, pose d'ongle, main, mains, pied, pieds, macon, mâcon, 71000, 71, graine de plaisir, détente, bien être, bien etre, soin, onglerie, soins, salon, moderne, prothésie, convivialité" />
+
 <link rel="shortcut icon" type="image/png" href="images/kaleessi/favicon-kaleessi-pink-black.png" />
 <link rel="stylesheet"    type="text/css"  href="css/reset.css"       media="screen" />
 <link rel="stylesheet"    type="text/css"  href="css/style.css"       media="screen" />
@@ -8,3 +13,21 @@
 <link rel="stylesheet"    type="text/css"  href="css/slider.css"      media="screen" />
 <link rel="stylesheet"    type="text/css"  href="css/prettyPhoto.css" media="screen" />
 
+<script>
+  (
+    function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    }
+  )
+
+  (window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-44824425-1', 'kaleessi.fr');
+  ga('send', 'pageview');
+</script>
