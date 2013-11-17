ddd0b57dc99ea78515910a6e57806c99460ef295
Jerome Guilloux <jguilloux@genymobile.com>
2013-11-17 14:39:51 +0100

    Change "normaux" by "en mauvais état"

--- site/www/include/welcome.php
+++ site/www/include/welcome.php
@@ -4,7 +4,7 @@
     <p class="color-1">Kaleessi, le plaisir jusqu'au bout des doigts</p>
     <p class="color-2">
     A la fronti&eacute;re de Macôn et Charnay, venez sublimer vos ongles dans votre nouveau salon moderne.
-    Ongles cassés, abimés, rongés ou normaux, nous utiliserons les meilleures techniques afin de les illuminer.
+    Ongles cassés, abimés, rongés ou en bon état, nous utiliserons les meilleures techniques afin de les illuminer.
     Laisser vous également séduire par notre espace boutique avec la vente de produits bien-être, chics et glamours.
     </p>
   </article>
