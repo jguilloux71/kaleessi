9e15b34fe85871bef1097aa526d8017fe08d725d
Jerome Guilloux <jguilloux@genymobile.com>
2013-12-04 21:14:43 +0100

    Telethon (END)

--- site/www/include/telethon.php
+++ site/www/include/telethon.php
@@ -1,15 +1,15 @@
 <div id="telethon" class="grid_12">
   <article class="block-1-content">
-    <p class="color-1">Kaleessi participe au Téléthon 2013 à Mâcon !</p>
+    <p class="color-1">Kaleessi, votre spécialiste ongulaire, participe au Téléthon 2013 à Mâcon !</p>
     <br/>
     <p class="color-2">
-    <strong>De 15H à 20H, place Antonin Poncet</strong>
+    <strong>De 15H à 19H30, Espace Carnot à Maĉon</strong>
     </p>
     <br/>
     <p class="color-2">
     Le stand Kaleessi sera heureux de vous accueillir afin de vous faire
     une pose de vernis simple au prix exceptionnel de <strong>2 euros</strong> (au lieu de 8 euros prix boutique).
-    Profitez-en pour découvrir les coffrets cadeaux de Noël dédiés à la beauté des ongles, des mains et des pieds.
+    Profitez-en pour découvrir les coffrets cadeaux de Noël dédiés à la beauté des ongles, du corps.
     </p>
     <br/>
     <p class="color-2">
