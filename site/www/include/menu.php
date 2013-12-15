fd25e7ac8f575871d32a06b5272b2b33fae53174
Jerome Guilloux <jguilloux@genymobile.com>
2013-12-15 17:28:09 +0100

    Box and gift certs in submenus

--- site/www/include/menu.php
+++ site/www/include/menu.php
@@ -4,11 +4,14 @@
     <li><a href="prices.php">Tarifs</a>
       <ul>
         <li><a href="data/kaleessi-grille-prestations-tarifs.pdf">Telecharger</a></li>
+        <li><a href="cheques-cadeaux.php">Cheques cadeaux</a></li>
       </ul>
     </li>
     <li><a href="saloon.php">Salon</a>
       <ul>
         <li><a href="photos.php">Photos</a></li>
+        <li><a href="coffrets.php">Coffrets</a></li>
+        <li><a href="cheques-cadeaux.php">Cheques cadeaux</a></li>
       </ul>
     </li>
     <li><a href="contact.php">Contact</a></li>
