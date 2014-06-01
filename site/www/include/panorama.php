13e4da439bb638d84b388616bd67b41b8f0c3568
Jerome Guilloux <jguilloux@genymobile.com>
2014-06-01 16:26:54 +0200

    Suppression de la photo pour le concours

--- site/www/include/panorama.php
+++ site/www/include/panorama.php
@@ -2,10 +2,6 @@
   <div class="slider">
     <ul class="items">
       <li>
-        <img src="images/kaleessi/panorama/vignette-concours.png" alt="">
-        <div class="banner slider-text"><strong>Concours Kaleessi</strong></div>
-      </li>
-      <li>
         <img src="images/kaleessi/panorama/miss-saone-et-loire.png" alt="">
         <div class="banner slider-text"><strong>Miss Saone-et-Loire 2013</strong></div>
       </li>
@@ -29,7 +25,6 @@
 
     <div class="pagination">
       <ul>
-        <li><a href="#"><img src="images/kaleessi/panorama/vignette-concours-small.png" alt=""></a></li>
         <li><a href="#"><img src="images/kaleessi/panorama/miss-saone-et-loire-small.png" alt=""></a></li>
         <li><a href="#"><img src="images/kaleessi/panorama/vernis-permanent-small.jpg" alt=""></a></li>
         <li><a href="#"><img src="images/kaleessi/panorama/french-manucure-small.jpg" alt=""></a></li>
