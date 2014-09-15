e45e13d144762f684603e681292ac3b3cedec26a
Jerome Guilloux <jguilloux@genymobile.com>
2014-09-15 13:23:58 +0200

    Changement des horaires du lundi et mercredi

--- site/www/include/hours.php
+++ site/www/include/hours.php
@@ -1,7 +1,7 @@
 <table class="color-6">
-  <tr><td>Lundi</td><td>&nbsp;: 14H00 - 18H00</td></tr>
+  <tr><td>Lundi</td><td>&nbsp;: 09H30 - 18H00 (non-stop)</td></tr>
   <tr><td>Mardi</td><td>&nbsp;: 09H30 - 18H00 (non-stop)</td></tr>
-  <tr><td>Mercredi</td><td>&nbsp;: 09H00 - 13H00</td></tr>
+  <tr><td>Mercredi</td><td>&nbsp;: Fermé</td></tr>
   <tr><td>Jeudi</td><td>&nbsp;: 09H30 - 18H00 (non-stop)</td></tr>
   <tr><td>Vendredi</td><td>&nbsp;: 09H30 - 18H00 (non-stop)</td></tr>
   <tr><td>Samedi</td><td>&nbsp;: 09H00 - 13H00</td></tr>
